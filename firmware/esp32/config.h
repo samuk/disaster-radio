a3ba1dd97870665112397a227d16803dfb9d454b
samuk <sam@bristolwireless.net>
2020-02-24 17:36:40 +0000

    Update config.h
    Added M5 stack board and uncommented the Tbeam settings

--- firmware/esp32/config.h
+++ firmware/esp32/config.h
@@ -34,27 +34,50 @@
 #endif
 
 #ifdef TTGO_TBEAM
-// #define LORA_SCK 14
-// #define LORA_MISO 2
-// #define LORA_MOSI 15
-// #define LORA_CS 18
-// #define LORA_RST 23
-// #define LORA_IRQ 26
-// #define SD_SCK 14
-// #define SD_MISO 2
-// #define SD_MOSI 15
+#define LORA_SCK 14
+#define LORA_MISO 2
+#define LORA_MOSI 15
+#define LORA_CS 18
+#define LORA_RST 23
+#define LORA_IRQ 26
+#define SD_SCK 14
+#define SD_MISO 2
+#define SD_MOSI 15
 #define OLED_SDA 21
 #define OLED_SCL 22
 #define OLED_RST NOT_A_PIN
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
-
 // https://github.com/cyberman54/ESP32-Paxcounter/blob/825520a20f1a1b7a9ac5e725484c2704bcb8117b/src/hal/ttgobeam.h
 #define GPS_SERIAL 9600, SERIAL_8N1, GPIO_NUM_12, GPIO_NUM_15
-
 #endif
 
-#ifdef HELTEC
+#ifndef _M5STACK_CORE
+#define LORA_SCK  SCK
+#define LORA_CS   SS
+#define LORA_MISO MISO
+#define LORA_MOSI MOSI
+#define LORA_RST  GPIO_NUM_36
+#define LORA_IRQ  GPIO_NUM_26
+#define LORA_IO1  GPIO_NUM_34 // must be externally wired on PCB!
+#define LORA_IO2  LMIC_UNUSED_PIN
+#define HAS_SDCARD  1      // this board has an SD-card-reader/writer
+// Pins for SD-card
+#define SDCARD_CS    GPIO_NUM_4
+#define SDCARD_MOSI  MOSI
+#define SDCARD_MISO  MISC
+#define SDCARD_SCLK  SCK
+// Pins for interface of LC Display
+#define MY_OLED_CS GPIO_NUM_14
+#define MY_OLED_DC GPIO_NUM_27
+#define MY_OLED_CLK GPIO_NUM_18
+#define MY_OLED_RST GPIO_NUM_33
+#define MY_OLED_BL GPIO_NUM_32
+#define MY_OLED_MOSI GPIO_NUM_23
+#define MY_OLED_MISO GPIO_NUM_23
+#endif
+
+#ifdef HELTEC //which Heltec?
 #define LORA_IRQ DIO0
 #define LORA_IO1 DIO1
 #define LORA_IO2 DIO2
